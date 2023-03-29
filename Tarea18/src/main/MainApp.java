8291c66995be8793c26d0ebb748bfbc1a60e1f81
camilagarcia36 <garciiaperezz@gmail.com>
2023-03-29 10:59:59 +0200

    primer commit

---
+++ Tarea18/src/main/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author camila
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
