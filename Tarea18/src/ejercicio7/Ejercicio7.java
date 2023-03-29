ba79c508627d06b054daba8928b2690d8b6da2da
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-29 23:13:32 +0200

    Ejercicio 7 y Ejercicio 8 Finalizado

--- Tarea18/src/ejercicio7/Ejercicio7.java
+++ Tarea18/src/ejercicio7/Ejercicio7.java
@@ -3,6 +3,8 @@
  */
 package ejercicio7;
 
+import connection.Connector;
+
 /**
  * @author aitor
  *
@@ -13,8 +15,32 @@ public class Ejercicio7 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connector c = new Connector("root", "root");
+		c.createDB("cientificos");
+		
+		c.createTable("cientificos", "cientificos", 
+				"(DNI varchar(8) NOT NULL,"
+				+ "NOMAPELS varchar(255) NOT NULL,"
+				+ "PRIMARY KEY(DNI));");
+		
+		c.createTable("cientificos", "proyecto", 
+				"(ID char(4) NOT NULL,"
+				+ "NOMBRE varchar(255) NOT NULL,"
+				+ "HORAS int NOT NULL,"
+				+ "PRIMARY KEY(ID));");
+		
+		
+		c.createTable("cientificos", "asignado_a", 
+				"(CIENTIFICO varchar(8) NOT NULL,"
+				+ "PROYECTO char(4) NOT NULL,"
+				+ "PRIMARY KEY(CIENTIFICO, PROYECTO),"
+				+ "FOREIGN KEY(CIENTIFICO) REFERENCES cientificos (DNI) ON DELETE CASCADE ON UPDATE CASCADE,"
+				+ "FOREIGN KEY(PROYECTO) REFERENCES proyecto (ID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		
+		c.insertData("cientificos", "cientificos", "DNI, NOMAPELS", "'12345678', 'Aitor Iglesias Pubill'");
+		c.insertData("cientificos", "proyecto", "ID, NOMBRE, HORAS", "10, 'Proyecto Java', 300");
+		c.insertData("cientificos", "asignado_a", "CIENTIFICO, PROYECTO", "'12345678', 10");
+		c.closeConnection();
 	}
 
 }
--- Tarea18/src/ejercicio8/Ejercicio8.java
+++ Tarea18/src/ejercicio8/Ejercicio8.java
@@ -3,6 +3,8 @@
  */
 package ejercicio8;
 
+import connection.Connector;
+
 /**
  * @author aitor
  *
@@ -13,8 +15,39 @@ public class Ejercicio8 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connector c = new Connector("root", "root");
+		c.createDB("grandes_almacenes");
+		
+		c.createTable("grandes_almacenes", "cajeros", 
+				"(CODIGO int NOT NULL,"
+				+ "NOMAPELS varchar(255) NOT NULL,"
+				+ "PRIMARY KEY(CODIGO));");
+		
+		c.createTable("grandes_almacenes", "productos", 
+				"(CODIGO int NOT NULL,"
+				+ "NOMBRE varchar(100) NOT NULL,"
+				+ "PRECIO int NOT NULL,"
+				+ "PRIMARY KEY(CODIGO));");
+		
+		c.createTable("grandes_almacenes", "maquinas_registradoras", 
+				"(CODIGO int NOT NULL,"
+				+ "PISO int NOT NULL,"
+				+ "PRIMARY KEY(CODIGO));");
+		
+		c.createTable("grandes_almacenes", "venta", 
+				"(CAJERO int NOT NULL,"
+				+ "MAQUINA int NOT NULL,"
+				+ "PRODUCTO int NOT NULL,"
+				+ "PRIMARY KEY(CAJERO, MAQUINA, PRODUCTO),"
+				+ "FOREIGN KEY(CAJERO) REFERENCES cajeros (CODIGO) ON DELETE CASCADE ON UPDATE CASCADE,"
+				+ "FOREIGN KEY(MAQUINA) REFERENCES maquinas_registradoras (CODIGO) ON DELETE CASCADE ON UPDATE CASCADE,"
+				+ "FOREIGN KEY(PRODUCTO) REFERENCES productos (CODIGO) ON DELETE CASCADE ON UPDATE CASCADE);");
+		
+		c.insertData("grandes_almacenes", "cajeros", "CODIGO, NOMAPELS", "10, 'Aitor Iglesias Pubill'");
+		c.insertData("grandes_almacenes", "productos", "CODIGO, NOMBRE, PRECIO", "101, 'Lavadora', 500");
+		c.insertData("grandes_almacenes", "maquinas_registradoras", "CODIGO, PISO", "50, 2");
+		c.insertData("grandes_almacenes", "venta", "CAJERO, MAQUINA, PRODUCTO", "10, 50, 101");
+		c.closeConnection();
 	}
 
 }
