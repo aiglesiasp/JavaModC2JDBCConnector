9ad5f204761c1365f3695659f33d2d72acec11a3
JPrimee <javierpsantos99@gmail.com>
2023-03-30 13:32:58 +0200

    Ejercicio 4

--- Tarea18/src/ejercicio4/Ejercicio4.java
+++ Tarea18/src/ejercicio4/Ejercicio4.java
@@ -1,20 +1,68 @@
-/**
- * 
- */
 package ejercicio4;
 
-/**
- * @author aitor
- *
- */
+import connection.Connector;
+
 public class Ejercicio4 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		final String USER = "root";
+		final String PASSWORD = "Pa$$w0rd";
+		
+		final String DATABASE = "Peliculas_y_salas";
+		
+		//Create a new connection to database
+		Connector c = new Connector(USER, PASSWORD);
+				
+		//Create the "Peliculas_y_salas" database
+		c.createDB(DATABASE);
+		
+		//Create tables for database
+		createTables(c, DATABASE);
+		
+		//Inserts for all tables
+		insertTables(c, DATABASE);
+			
+		//Close database connection
+		c.closeConnection();
+	}
+	
+	public static void createTables(Connector c, String DATABASE) {
+		//Create table "peliculas"
+		c.createTable(DATABASE, "peliculas", "(" 
+			+ "codigo INT AUTO_INCREMENT PRIMARY KEY,"
+			+ "nombre VARCHAR(100) NOT NULL,"
+			+ "calificaion_edad INT NOT NULL"
+			+ ");");
+				
+		//Create table "salas"
+		c.createTable(DATABASE, "salas", "("
+			+ "codigo INT AUTO_INCREMENT PRIMARY KEY,"
+			+ "nombre VARCHAR(100) NOT NULL,"
+			+ "pelicula INT NOT NULL,"
+			+ "FOREIGN KEY (pelicula) REFERENCES peliculas(codigo)"
+			+ "ON DELETE CASCADE ON UPDATE CASCADE"
+			+ ");");
 	}
 
+	public static void insertTables(Connector c, String DATABASE) {
+		//Insert into "peliculas"
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode I - The Phantom Menace', 12");
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode II - The Attack of the Clones', 12");
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode III - Revenge of the Sith', 12");
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode IV - A New Hope', 12");
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode V - The Empire Strikes Back', 12");
+		c.insertData(DATABASE, "peliculas", "nombre, calificaion_edad", "'STAR WARS: Episode VI - Return of the Jedi', 12");
+		
+		//Insert into "salas"
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 1', 4");
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 2', 5");
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 3', 6");
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 4', 1");
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 5', 2");
+		c.insertData(DATABASE, "salas", "nombre, pelicula", "'Sala 6', 3");
+	}
 }
